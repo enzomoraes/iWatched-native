c01356f643b1479c7897f750dbb2464e56b721e4
enzomoraes <enzomoraes12@hotmail.com>
2024-09-20 20:29:50 -0400

    chore: expo 51 tailwind setup

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from 'react-native';
+
+export default function HomeScreen() {
+  return (
+    <View className='flex-1 items-center justify-center px-4'>
+      <Text className='text-yellow-500'>Welcome!!</Text>
+    </View>
+  );
+}
